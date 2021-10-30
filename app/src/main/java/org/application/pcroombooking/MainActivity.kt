3b59751bb7adc1a485b24a21748d9ea7aebaae17
SonMinGyu <littleking13@naver.com>
2021-10-30 14:15:37 +0900

    Init commit.
    First commit.

---
+++ app/src/main/java/org/application/pcroombooking/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.application.pcroombooking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
